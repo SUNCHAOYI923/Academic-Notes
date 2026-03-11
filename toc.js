faa4c85e0f3fe35582e5bed1d2ee26f50affa605
SUNCHAOYI923 <3187756748@qq.com>
2026-03-11 16:18:45 +0800

    Add catalogue.

--- toc.js
+++ toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="CSC3060/Notes.html"><strong aria-hidden="true">1.</strong> CSC3060</a></li><li class="chapter-item expanded "><a href="CSC3060/Notes_CSAPP.html"><strong aria-hidden="true">2.</strong> CSC3060 Reference</a></li><li class="chapter-item expanded "><a href="CSC4303/Notes.html"><strong aria-hidden="true">3.</strong> CSC4303</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="Catalogue.html"><strong aria-hidden="true">1.</strong> Catalogue</a></li><li class="chapter-item expanded "><a href="CSC3060/Notes.html"><strong aria-hidden="true">2.</strong> CSC3060</a></li><li class="chapter-item expanded "><a href="CSC3060/Notes_CSAPP.html"><strong aria-hidden="true">3.</strong> CSC3060 Reference</a></li><li class="chapter-item expanded "><a href="CSC4303/Notes.html"><strong aria-hidden="true">4.</strong> CSC4303</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0];
         if (current_page.endsWith("/")) {
